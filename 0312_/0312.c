0281b0bb78fd7ccfb2942f02eeec9311821657d0
mean12345 <jiji2016@naver.com>
2024-03-12 10:17:55 +0900

    0부터 100까지 소수의 합

--- 0312_/0312.c
+++ 0312_/0312.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 int main() {
-	int sum = 1;
-	
+	int sum = 0;
 	
 	for (int i = 2; i <= 100; i++) {
 		int count = 0;
 		for (int j = 2; j < i; j++) {
 			if (i%j == 0) {
 				count++;
+				break;
 			}
 		}
-		if (count > 0) {
+		if(count == 0){
 			sum += i;
 		}
 	}
